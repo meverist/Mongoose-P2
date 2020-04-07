3ce97b8579bd9f3e22a078f43840f608e1b837f6
Jacen <jbwyke@yahoo.com>
2020-04-07 16:54:29 -0400

    added basic employeeAddPet form

---
+++ PetAdoptionNG/src/app/components/employee-add-pet/employee-add-pet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-add-pet',
+  templateUrl: './employee-add-pet.component.html',
+  styleUrls: ['./employee-add-pet.component.css']
+})
+export class EmployeeAddPetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
