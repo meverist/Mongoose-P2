b5853784fa6a3f8ef7aa890ff186a6d6f10f4ddc
Zombie X Knight <ciscoradilla@yahoo.com>
2020-04-08 17:32:00 -0500

    registration

--- PetAdoptionNG/src/app/app.module.ts
+++ PetAdoptionNG/src/app/app.module.ts
@@ -4,20 +4,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { UserregisterComponent } from './components/userregister/userregister.component';
-=======
-import { ViewApplicationsComponent } from './components/view-applications/view-applications.component';
->>>>>>> 1641dcf64e5a04dae996c4df68ca5e7dddc16363
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     UserregisterComponent,
-=======
-    ViewApplicationsComponent
->>>>>>> 1641dcf64e5a04dae996c4df68ca5e7dddc16363
   ],
   imports: [
     BrowserModule,
