bda10d350d79be72b10a18fe109ca27302ffb888
Zombie X Knight <ciscoradilla@yahoo.com>
2020-04-08 15:54:06 -0500

    4-8-20

--- PetAdoptionNG/src/app/app.module.ts
+++ PetAdoptionNG/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms'
+
 import { AppComponent } from './app.component';
+import { UserregisterComponent } from './components/userregister/userregister.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserregisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ PetAdoptionNG/src/app/models/Adoptor.ts
@@ -0,0 +1,16 @@
+export class Adopter {
+
+    uname :string;
+    email :string;
+    password :string;
+    role :string;
+
+    constructor(uname :string, email :string, password :string, role :string) {
+        this.uname = uname;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+
+}
