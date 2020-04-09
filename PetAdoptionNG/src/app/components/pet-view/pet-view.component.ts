0a45e5d83326eb5c75d16c609acb2551f9f763cb
mcnabb579 <mcnabb579@gmail.com>
2020-04-09 16:09:16 -0400

    fix

--- PetAdoptionNG/src/app/components/pet-view/pet-view.component.ts
+++ PetAdoptionNG/src/app/components/pet-view/pet-view.component.ts
@@ -15,11 +15,11 @@ index: number = 0;
  
   ngOnInit(): void {
 
-let pet1: Pet = new Pet(1,"Bo","dog","lab",3,50,"healthy","I love to play","pic.com");
-let pet2: Pet = new Pet(1,"Zo","doog","laab",30,55,"healthys","I love to plays","pic.coms");
+//let pet1: Pet = new Pet(1,"Bo","dog","lab",3,50,"healthy","I love to play","pic.com");
+//let pet2: Pet = new Pet(1,"Zo","doog","laab",30,55,"healthys","I love to plays","pic.coms");
 
-let pet3: Pet = new Pet(1,"Lo","dooog","laaab",31,56,"healthyss","I love to playss","pic.comss");
-this.pets.push(pet1,pet2,pet3);
+//let pet3: Pet = new Pet(1,"Lo","dooog","laaab",31,56,"healthyss","I love to playss","pic.comss");
+//this.pets.push(pet1,pet2,pet3);
   }
     nextPet() {
    if(this.index==this.pets.length-1){
