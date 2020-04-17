fe62ca0b99343ab4bcd14f45057eefaab98eea67
Mathew <mat.everist@gmail.com>
2020-04-17 16:02:26 -0400

    Changed dependency

--- PetAdoption/src/main/java/dev/mongoose/driver/PetAdoptionApplication.java
+++ PetAdoption/src/main/java/dev/mongoose/driver/PetAdoptionApplication.java
@@ -3,6 +3,7 @@ package dev.mongoose.driver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("dev.mongoose")
 @EnableJpaRepositories("dev.mongoose.repositories")
 @EntityScan("dev.mongoose.models")
-public class PetAdoptionApplication {
+public class PetAdoptionApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetAdoptionApplication.class, args);
